0bcb39ca7e2cd113384517ae8c9c168bb74eea7f
R Vijaya Bhaskar <vijay.smartph@gmail.com>
2018-10-16 15:32:37 +0530

    init setup
    Test pro to check the Inetellij  settings

---
+++ cppJNI/src/cppJNI/Factorial.java
@@ -0,0 +1,32 @@
+package cppJNI;
+
+public class Factorial {
+
+    private int num;
+    public void SetNumber(int n) {
+
+        num = n;
+    }
+
+    public int getNum(){
+        return num;
+
+    }
+
+    long computeFact(){
+
+        int n = 1;
+        for(int i=1; i<= getNum(); i++)
+            n *= i;
+
+        return n;
+    }
+
+    public static void main(String[] arg){
+
+        Factorial f = new Factorial();
+        f.SetNumber(5);
+        long n = f.computeFact();
+        System.out.print(n);
+    }
+}
